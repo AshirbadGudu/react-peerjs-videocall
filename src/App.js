d9ae70ad654dfa387fe0e84b0c4c53104ab69636
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-08-06 22:13:03 +0530

    Video Call Feature Implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Peer from "peerjs";
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+const YOUR_RANDOM_ID = `id-${parseInt(Math.random() * 100)}`;
+const MEDIA_STREAM_CONSTRAINTS = {
+  video: true,
+  audio: {
+    echoCancellation: true,
+    noiseSuppression: true,
+  },
+};
 function App() {
+  const localVideoRef = useRef();
+  const remoteVideoRef = useRef();
+  const inputVideoRef = useRef();
+  const startVideoCall = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(
+        MEDIA_STREAM_CONSTRAINTS
+      );
+      addLocalVideo(stream);
+      let call = peerRef.current.call(inputVideoRef.current.value, stream);
+      call.on("stream", (remoteStream) => addRemoteVideo(remoteStream));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const [peerID, setPeerID] = useState("");
+  const [isOnCall, setIsOnCall] = useState(false);
+  const peerRef = useRef();
+
+  // Create Receive Function
+  const receiveCall = async (call) => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(
+        MEDIA_STREAM_CONSTRAINTS
+      );
+      addLocalVideo(stream);
+      call.answer(stream);
+      call.on("stream", (remoteStream) => addRemoteVideo(remoteStream));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Create Remote Stream Video
+  const addRemoteVideo = (remoteStream) => {
+    remoteVideoRef.current.srcObject = remoteStream;
+    setIsOnCall(true);
+  };
+
+  // Create local Stream Video
+  const addLocalVideo = (localStream) =>
+    (localVideoRef.current.srcObject = localStream);
+
+  // Stop Video Call
+  const endVideoCall = () => {
+    const localTracks = localVideoRef.current.srcObject.getTracks();
+    localTracks.forEach((track) => track.stop());
+    const remoteTracks = localVideoRef.current.srcObject.getTracks();
+    remoteTracks.forEach((track) => track.stop());
+    localVideoRef.current.srcObject = null;
+    remoteVideoRef.current.srcObject = null;
+    setIsOnCall(false);
+  };
+
+  useEffect(() => {
+    peerRef.current = new Peer(YOUR_RANDOM_ID);
+    // Listen Peer Open Event
+    peerRef.current.on("open", (id) => {
+      setPeerID(id);
+    });
+    // Listen Peer Call Event
+    peerRef.current.on("call", receiveCall);
+    return () => {};
+  }, []);
+
+  const copyOnClipboard = (txt) => navigator.clipboard.writeText(txt);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header>
+        <h2>Your Video Call id: {peerID}</h2>
+        <button
+          onClick={() => {
+            copyOnClipboard(peerID);
+          }}
         >
-          Learn React
-        </a>
+          copy
+        </button>
       </header>
+      <main>
+        <video ref={localVideoRef} autoPlay></video>
+        <video ref={remoteVideoRef} autoPlay></video>
+      </main>
+      <footer>
+        <input type="text" ref={inputVideoRef} />
+        {isOnCall ? (
+          <button className="end" onClick={endVideoCall}>
+            End Call
+          </button>
+        ) : (
+          <button className="start" onClick={startVideoCall}>
+            Call
+          </button>
+        )}
+      </footer>
     </div>
   );
 }
